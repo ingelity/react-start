a2f56494d1379a7af418d388332ce07962f40a39
Aleksandar Jovanovic <ingelity@gmail.com>
2016-11-24 11:36:34 +0900

    updated hot-module-reloading

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -40,7 +40,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loaders: ['react-hot', 'babel'],
+        loaders: ['babel'],
       },
       {
         test: /\.scss$/,
